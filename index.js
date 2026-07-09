c6c8717a21d4ee85cc566524a77d32590f14f849
Janee <janeemcneil@outlook.com>
2026-07-09 12:35:03 -0600

    change pg.pool back to pg.client

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.static("public"));
 
 // connect to database
 
-const db = new pg.Pool({
+const db = new pg.Client({
   user: `${process.env.USER}`,
   database: `${process.env.DATABASE}`,
   host: `${process.env.HOST}`,
